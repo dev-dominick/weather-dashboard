4dd0aa38c776f2b2baf21c2f4595e8fdc155c4d5
Dom <Domalbano35@gmail.com>
2022-03-28 17:53:37 -0400

    working on api calling

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,23 @@
-let api = `https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}`
+let api = `https://api.openweathermap.org`
 let apiKey = `5eaec850f935aee63f8f7d4cd1fb80d7`
+let fetchLocation = document.getElementById("search-btn")
+
+
+https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
+
+function getApi(){
+  let inputInfo = fetchLocation.value.trim();
+  let apiUrl = `${api}/data/2.5/weather?q=${inputInfo}&appid=${apiKey}`
+console.log(getApi)
+  fetch(apiUrl)
+    .then(function(response){
+      return response.json();
+      console.log(response)
+    })
+
+
+};
+
+fetchLocation.addEventListener("submit", getApi)
 
 
