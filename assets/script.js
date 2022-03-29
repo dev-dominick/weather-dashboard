bc9c4807c7d310d9508787c341151d9acf2eb7a0
Dom <Domalbano35@gmail.com>
2022-03-28 22:28:53 -0400

    trying to get weather data to show on app

--- assets/script.js
+++ assets/script.js
@@ -1,28 +1,35 @@
 let api = 'https://api.openweathermap.org';
 let apiKey = "5eaec850f935aee63f8f7d4cd1fb80d7";
-let city = document.getElementById("#search-btn");
+let btn = document.getElementById("btn");
+let input = document.querySelector("#city-search")
+// console.log(input.value);
+btn.addEventListener("click", getApi);
 
-
-
-
-function getApi(){
+function getApi(e){
   // let inputInfo = fetchLocation.value.trim();
-  var apiUrl = `${api}/data/2.5/weather?q=${city}&appid=${apiKey}`;
-
+  console.log(e);
+  var apiUrl = `${api}/data/2.5/weather?q=${input.value}&appid=${apiKey}`;
+  console.log(input.value);
   fetch(apiUrl)
     .then(function(response){
       return response.json();
+    
     })
     .then(function (weather){
-      for (let i = 0; i < weather.length; i++) {
-        let listItem = weather[i]
-        console.log(weather);
+      console.log(weather);
+      weather.main.temp
+      console.log(weather.main.temp);
+      document.getElementById("city-date")
+      
+      // for (let i = 0; i < weather.length; i++) {
+      //   let listItem = weather[i]
+      //   console.log(weather);
         
-      }
+      // }
     });
 
 };
 
-city.addEventListener("click", getApi())
+// btn.addEventListener("click", getApi)
 
 
