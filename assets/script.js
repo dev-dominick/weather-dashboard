28d33dffcbc57ac7803a4b064cf38e287ac3450f
Dom <Domalbano35@gmail.com>
2022-03-28 23:00:12 -0400

    dynamically displayed weather data info to weather container

--- assets/script.js
+++ assets/script.js
@@ -2,6 +2,7 @@ let api = 'https://api.openweathermap.org';
 let apiKey = "5eaec850f935aee63f8f7d4cd1fb80d7";
 let btn = document.getElementById("btn");
 let input = document.querySelector("#city-search")
+let weatherContainer = document.getElementById('weather-container')
 // console.log(input.value);
 btn.addEventListener("click", getApi);
 
@@ -19,7 +20,27 @@ function getApi(e){
       console.log(weather);
       weather.main.temp
       console.log(weather.main.temp);
-      document.getElementById("city-date")
+      let cityName = document.createElement('h3')
+      cityName.textContent = `${input.value}`
+      weatherContainer.append(cityName)
+      let temp = document.createElement('p')
+      temp.textContent = weather.main.temp
+      weatherContainer.append(temp)
+      let wind = document.createElement("p");
+      wind.textContent = weather.wind.speed
+      weatherContainer.append(wind)
+      let humidity = document.createElement("p");
+      humidity.textContent = weather.main.humidity
+      weatherContainer.append(humidity)
+      let uv = document.createElement('p')
+      uv.textContent = weather.clouds.all
+      weatherContainer.append(uv)
+
+
+
+
+      // CountQueuingStrategy.textContent = ${input.value}
+      // weatherContainer.append
       
       // for (let i = 0; i < weather.length; i++) {
       //   let listItem = weather[i]
