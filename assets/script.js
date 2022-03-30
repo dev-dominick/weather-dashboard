04a65bd6fd153546ed15210eea20025380fbbebe
Dom <Domalbano35@gmail.com>
2022-03-30 19:12:18 -0400

    logic changed in js

--- assets/script.js
+++ assets/script.js
@@ -44,7 +44,7 @@ function getApi(){
      
     function weatherFetch(location){
       let { lat, lon } = location;
-      let city = location.name;
+      // let city = location;
       
     
       let oneCall = `${api}/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=minutely,hourly&appid=${apiKey}`;
@@ -149,24 +149,6 @@ searchStorage.addEventListener("click", searchClick);
 
 
 
-// GIVEN a weather dashboard with form inputs
-// WHEN I search for a city
-// THEN I am presented with current and future conditions for that city and that city is added to the search history
 
-
-// WHEN I view current weather conditions for that city
-// THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
-
-
-// WHEN I view the UV index
-// THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
-
-
-// WHEN I view future weather conditions for that city
-// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
-
-
-// WHEN I click on a city in the search history
-// THEN I am again presented with current and future conditions for that city
 
 
